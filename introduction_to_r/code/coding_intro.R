179931b2a01551b96222c0da853f1b17cc9d0ef8
christina_m <christina.mccosker@maine.edu>
2024-02-14 14:36:28 -0500

    update

---
+++ introduction_to_r/code/coding_intro.R
@@ -0,0 +1,20 @@
+##### Purpose ####
+# This script is to provide an introduction to E coding for beginners
+
+
+#### Co-Authors ####
+# Christina McCosker
+# Julia Sunnarborg
+# Alice Hotopp
+# Dara Yiu
+
+
+#### Coding Basics ####
+
+# This document is a script, it is essentially a "word doc" full of code in R langauge that you can run
+# Use separate scripts for different analyses, research projects or questions, etc.
+
+# This is a hashtag that makes your line of text a "comment" in R, this is text that you can write to
+# annotate your script and take notes - anything you don't want run as code.
+
+
